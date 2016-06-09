e51b9e0d0b86cd383971f81b3c14c9d3a0d5aeba
shawnfc <shawn@shawnconrad.com>
2016-06-09 17:33:16 -0400

    added files

---
+++ js/app.js
@@ -0,0 +1,72 @@
+angular.module('scApp', ['ngMaterial', 'ui.router'])
+    .run(function ($rootScope, $state, $stateParams) {
+        $rootScope.$on('$stateChangeStart',
+            function (event, toState, toParams, fromState, fromParams, options) {
+                //event.preventDefault();
+                $rootScope.backgroundCss = toState.name;
+            });
+
+    })
+    .config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('red', {
+                'default': '600', // by default use shade 400 from the pink palette for primary intentions
+                'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+                'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+                'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+            })
+            // If you specify less than all of the keys, it will inherit from the
+            // default shades
+            .accentPalette('grey', {
+                'default': '800' // use shade 200 for default, and keep all other shades the same
+            });
+
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/home");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "views/home.html"
+            })
+            .state('about', {
+                url: "/about",
+                templateUrl: "views/about.html"
+            })
+            .state('portfolio', {
+                url: "/portfolio",
+                templateUrl: "views/portfolio.html"
+            })
+            .state('contact', {
+                url: "/contact",
+                templateUrl: "views/contact.html"
+            })
+    })
+    .controller("MyController", ["$scope", "$http", function MyController($scope, $http) {
+        $http.get('js/data.json').success(function (data) {
+            $scope.portfolio = data;
+            $scope.portfolioOrder = "name";
+        });
+    }]);
+
+"use strict";
+
+var burger = document.getElementById("burger-button");
+
+var links = document.getElementsByClassName("links");
+
+burger.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.body.classList.toggle("open");
+    burger.classList.toggle("open");
+});
+
+for (var i = 0; i < links.length; i++) {
+    console.log(links[0]);
+    links[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        document.body.classList.toggle("open");
+        burger.classList.toggle("open");
+    }, false);
+}
